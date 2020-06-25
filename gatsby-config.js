797bef0b417281ab15d930b92e9227d5a2756f58
Psikoi <ruben.amendoeira@gmail.com>
2020-06-25 15:38:54 +0100

    Fix gatsby manifest icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,12 +21,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `Wise Old Man - Discord Bot`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/icon.png` // This path is relative to the root of the site.
       }
     }
   ]
