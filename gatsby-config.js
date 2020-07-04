00c90a7e3514a6197e1e0e7c34ea9b6339490233
Psikoi <ruben.amendoeira@gmail.com>
2020-07-05 00:35:31 +0100

    Change theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `Wise Old Man - Discord Bot`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#121212`,
+        theme_color: `#121212`,
         display: `minimal-ui`,
         icon: `src/images/icon.png` // This path is relative to the root of the site.
       }
