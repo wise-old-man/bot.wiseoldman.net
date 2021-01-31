cd224f3d6886093d545dcdc75c8793a02f8a1d25
Psikoi <ruben.amendoeira@gmail.com>
2021-01-31 09:43:58 +0000

    Add custom time period options to player gained

--- src/config/commands/players/gained.ts
+++ src/config/commands/players/gained.ts
@@ -3,9 +3,15 @@ export default {
   baseCommand: '!gained {username}',
   options: [
     {
-      flag: '--6h / --day / --week / --month / --year',
+      flag: '--6h / --day / --week / --month / --year / --2w6d5h (see examples)',
       description: 'Defines the time period for the player gains.'
     }
   ],
-  examples: ['!gained Zezima', '!gained Psikoi --month', '!gained Cometz --day']
+  examples: [
+    '!gained Zezima',
+    '!gained Psikoi --month',
+    '!gained Cometz --day',
+    '!gained Lynx Titan --1y5m2h',
+    '!gained Iron Mammal --6m2w5h'
+  ]
 };
